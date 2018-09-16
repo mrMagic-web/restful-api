13f785ed27ba20eeb9d001cbc8b4bafba1fae3ea
mrMagic-web <maciejdrahusz@gmail.com>
2018-09-16 11:11:21 +0200

    Update

---
+++ rest-api-notes.js
@@ -0,0 +1,10 @@
+// -----------------------------------
+// ------ REST-api with Mongodb ------
+// -----------------------------------
+// Install mongodb on the computer
+// npm i --save body-parser express mongoose nodemon
+// npm i --save-dev babel-cli babel-preset-es2015 babel-preset-stage-0
+// Setup .babelrc { "presets": [ "env", "stage-0" ]}
+
+// RESTful APIs - Using HTTP protocols to do transactions with a backend
+// Using GET, POST, PUT and DELETE calls to the backend
